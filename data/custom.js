9e2fbe72bed1fb175cb1891f1beab379fd7302dc
Jahrul Novario <73051140+jahrulnr@users.noreply.github.com>
2021-12-31 21:11:46 +0700

    update

--- data/custom.js
+++ data/custom.js
@@ -1,11 +1,25 @@
 $(document).ready(function(){
   $('.bio').click(function(){
-    $.ajax(
-      {
-        url: "./data/json/bio.json", 
-        success: function(result){
-          $("#bio .modal-body").html(JSON.parse(result));
-        }
-      });
+    $.ajax({
+      url: "./data/json/kontak.json",
+      success: function(result) {
+        var data  = result;
+        var img   = '<img src="'+data['image']+'" width="100" height="100" class="rounded-circle">';
+        var email = '<span class="fa fa-envelope"></span> Email : <a href="mailto:' + data['email'] + '">' + data['email'] + '</a>';
+        var ig    = '<span class="fa fa-instagram"></span> Instagram : <a href="//instagram.com/' + data['instagram'] + '" target="_blank">@' + data['instagram'] + '</a>';
+
+        // merge
+        var kontak = "<center>" + img + "<br/>" + email + "<br/>" + ig + "</center><hr/>";
+
+        $("#bio .modal-body div").html(kontak);
+      }
+    });
+
+    $.ajax({
+      url: "./data/json/info.json", 
+      success: function(result){
+        $("#bio .modal-body p").html(JSON.parse(result));
+      }
+    });
   });
 });
