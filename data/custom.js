8cf38249fc704a02f7c0eef7df6406456d4680a5
Jahrul Novario <73051140+jahrulnr@users.noreply.github.com>
2021-12-31 23:03:34 +0700

    update final

--- data/custom.js
+++ data/custom.js
@@ -1,3 +1,14 @@
+$body = $("body");
+
+$(document).on({
+  ajaxStart: function() { 
+    $body.addClass("loading");    
+  },
+  ajaxStop: function() { 
+    $body.removeClass("loading"); 
+  }    
+});
+
 var d = new Date();
 $("#year").html(d.getFullYear());
 
@@ -7,7 +18,7 @@ $(document).ready(function(){
       url: "./data/json/kontak.json",
       success: function(result) {
         var data  = result;
-        var img   = '<img src="'+data['image']+'" width="100" height="100" class="rounded-circle">';
+        var img   = '<img src="'+data['image']+'" width="100" height="100" class="rounded-circle" id="profil-img">';
         var email = '<span class="fa fa-envelope"></span> Email : <a href="mailto:' + data['email'] + '">' + data['email'] + '</a>';
         var ig    = '<span class="fa fa-instagram"></span> Instagram : <a href="//instagram.com/' + data['instagram'] + '" target="_blank">@' + data['instagram'] + '</a>';
 
